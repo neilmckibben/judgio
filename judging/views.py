5bd02d3df2acf9c15272014c2dce2aa3e7f50a90
neilmckibben-osu <mckibben.64@osu.edu>
2019-07-11 00:09:33 -0700

    basic view for team form

--- judging/views.py
+++ judging/views.py
@@ -1,1 +1,15 @@
+from django.views.generic import CreateView
+from .models import Team
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import CreateView
+from .forms import TeamForm
 
+def register(request):
+
+
+    #really basic, will add more later
+    form = TeamForm(request.POST)
+    if form.is_valid():
+        form.save()
+    return render(request, 'form.html', {'form' : form})
